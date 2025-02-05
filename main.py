45cfef724d88d4b309e820322cf04c3bd988893d
Murilo-ZC <carvalho.zanini@gmail.com>
2025-02-05 10:02:25 -0300

    adicionando primeira versao de CLI

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Ola Mundo!!")
-print("Teste Colisão!")
+import sys
+
+# Cria uma função para executar o programa principal
+def main():
+    print(f"Dados Enviados: {sys.argv[0]}")
+    print(f"Arg enviados: {sys.argv}")
+
+
+# Verifica se o programa é o principal
+if __name__ == "__main__":
+    main()
